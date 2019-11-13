a9de9824a1aaef0f2ce750101bee2067e5e941a7
Ali Shalabi <ashalabi13@gmail.com>
2019-11-13 14:13:10 -0800

    Finished employees handler

--- api/app/handler/employees.go
+++ api/app/handler/employees.go
@@ -67,3 +67,45 @@ func DeleteEmployee(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
   }
   respondJSON(w, http.StatusNoContent, nil)
 }
+
+func DisableEmployee(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+
+  name := vars["name"]
+  employee := getEmployeeOr404(db, name, w, r)
+  if employee == nil {
+    return
+  }
+  employee.Disable()
+  if err := db.Save(&employee).Error; err != nil {
+    respondError(w, http.StatusInternalServerError, err.Error())
+    return
+  }
+  respondJSON(w, http.StatusOK, employee)
+}
+
+func EnableEmployee(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+
+  name := vars["name"]
+  employee := getEmployeeOr404(db, name, w, r)
+  if employee == nil {
+    return
+  }
+  employee.Enable()
+  if err := db.Save(&employee).Error; err != nil {
+    respondError(w, http.StatusInternalServerError, err.Error())
+    return
+  }
+  respondJSON(w, http.StatusOK, employee)
+}
+
+
+func getEmployeeOr404(db *gorm.DB, name string, w http.ResponseWriter, r *http.Request) *model.Employee {
+  employee := model.Employee{}
+  if err := db.First(&employee, model.Employee{Name: name}).Error; err != nil {
+    respondError(w, http.StatusNotFound, err.Error())
+    return nil
+  }
+  return &employee
+}
